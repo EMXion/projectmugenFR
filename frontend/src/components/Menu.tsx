c66159e176072e469c7b18c5c54200969b99bc12
PICQUET Maxime <maximepic256@gmail.com>
2024-02-18 18:51:21 +0100

    Add menu

---
+++ frontend/src/components/Menu.tsx
@@ -0,0 +1,20 @@
+function Menu() {
+    return(
+        <>
+            <iframe src="https://discord.com/widget?id=1176437832286474290&theme=dark" width="350" height="500" frameBorder={0} sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+            <div className="welcome">
+                <h1>
+                    Bienvenue investigateur<br/>
+                    dans la communauté française<br/>
+                    de Project Mugen !
+                </h1>
+            </div>
+            
+            <div className="button">
+                <a href="https://discord.com/invite/taRtHmEyQc"><h3>Rejoignez notre discord !</h3></a>
+            </div>
+        </>
+    );
+}
+
+export default Menu;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header.tsx"
+import Menu from "../components/Menu.tsx"
 
 function Home() {
     return (
         <>
             <Header />
+            <Menu />
         </>
     );
 }
